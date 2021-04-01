9fe710ac659ea65d307d2282580fac72b0cbf3e2
pankajmaran <pankajmaran@gmail.com>
2021-04-01 20:16:36 +0530

    Revert "riverting the first commit i.e modules in nodes"
    This reverts commit 427ce6e82d5fdace89ef06e7f288fe143aea11b2.

--- logger.js
+++
@@ -1,7 +0,0 @@
-var url = 'https://www.msn.com/en-in/weather/today/Bhopal,MP,India/we-city?el=9Em%2FXp6ggo5liMMhch%2B1TxPpEdv0nfecYmp%2FSxutl1WriiL02FeILh4%2FifYufkWJH9ZIn0OS3BiH70RERxqcMM2CBkS7L5QPv6BjmnnY95w%3D&weadegreetype=C&ocid=msedgntp';
-// console.log(module);
-var log= function(msg){
-    console.log('msg is '+ msg);
-}
-module.exports.log_fuction=log;
-
