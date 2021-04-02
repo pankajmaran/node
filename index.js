7a309185391411f65e946d1cf9f68486084db29a
pankajmaran <pankajmaran@gmail.com>
2021-04-02 22:10:42 +0530

    handling http request

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express= require('express');
 const app= new express();
-
+const courses=[
+    { id :1, subject : 'c1'},
+    { id :2, subject : 'c2'},
+    { id :3, subject : 'c3'},
+];
 app.get('/', (req, res)=>{
     // console.log('Heyyyy :)');
     res.send('Heyyyy :::))) ');
@@ -11,7 +15,12 @@ app.get('/api/courses', (req, res)=>{
 });
 app.get('/api/courses/:id/:subject', (req, res)=>{
     //var para=req.params;
-    res.send(req.params);
+    var course=courses.find( function(c){
+       return  c.id === parseInt(req.params.id) && c.subject===req.params.subject;
+    });
+    if(!course) res.status(404).send(" course in not available");
+    res.send(course);
+    // res.send(req.params);
     
 });
 app.get('/api/courses/:id/:subject/:name', (req, res)=>{
