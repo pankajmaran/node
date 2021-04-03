2af97ca7b5b32c51e1d6ba71687c3a2de31f17ab
pankajmaran <pankajmaran@gmail.com>
2021-04-03 16:52:42 +0530

    making post request with post man

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi=require('joi');
 const express= require('express');
 const app= new express();
+app.use( express.json());
+// app.use()
 const courses=[
     { id :1, subject : 'c1'},
     { id :2, subject : 'c2'},
@@ -29,8 +32,28 @@ app.get('/api/courses/:id/:subject/:name', (req, res)=>{
     res.send(req.query);
     
 });
+app.post('/api/courses', (req, res)=>{
+    if( undefined === req.body.subject  || req.body.subject.length <2)
+    {
+        res.status(400).send('error in subject name'+ req.body.subject);
+        return;
+    }
+    const course={
+         id : courses.length+1,
+          subject: req.body.subject
+        
+    };
+    const schema={
+        subject : Joi.string().min(2).required()
+    };
+    // const result = Joi.valid(req.body, schema);
+    // console.log(result);
+    courses.push(course);
+    res.send(course);
+});
 //port
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(` listeing on port ${port}`);
 });
+
